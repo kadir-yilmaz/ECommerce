511f331a8c55f36ede46c22a9032fdb782923b1a
Kadir YILMAZ <kadiryilmaz19821@gmail.com>
2026-06-05 02:24:17 +0300

    d1

--- ECommerce.Persistence/Configuration.cs
+++ ECommerce.Persistence/Configuration.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.Configuration;
+using System.IO;
+using System;
 
 namespace ECommerce.Persistence
 {
@@ -10,11 +12,28 @@ namespace ECommerce.Persistence
             {
                 ConfigurationManager configurationManager = new();
 
-                // WebAPI projesinin appsettings.json dosyasını bul
-                string basePath = Path.Combine(Directory.GetCurrentDirectory(), "../../ECommerce.WebAPI");
-                
-                if (!Directory.Exists(basePath))
-                    basePath = Directory.GetCurrentDirectory();
+                string basePath = AppContext.BaseDirectory;
+
+                // Robust path resolution for appsettings.json:
+                // 1. Check AppContext.BaseDirectory (where binaries are deployed, e.g. IIS publish folder or bin/Debug/...)
+                // 2. Check Directory.GetCurrentDirectory()
+                // 3. Check relative path to WebAPI project from CurrentDirectory (for EF Migrations run from Persistence folder)
+                // 4. Check relative path to WebAPI project from BaseDirectory
+                if (!File.Exists(Path.Combine(basePath, "appsettings.json")))
+                {
+                    if (File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json")))
+                    {
+                        basePath = Directory.GetCurrentDirectory();
+                    }
+                    else if (File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "../../ECommerce.WebAPI/appsettings.json")))
+                    {
+                        basePath = Path.Combine(Directory.GetCurrentDirectory(), "../../ECommerce.WebAPI");
+                    }
+                    else if (File.Exists(Path.Combine(AppContext.BaseDirectory, "../../../ECommerce.WebAPI/appsettings.json")))
+                    {
+                        basePath = Path.Combine(AppContext.BaseDirectory, "../../../ECommerce.WebAPI");
+                    }
+                }
 
                 configurationManager.SetBasePath(basePath);
                 configurationManager.AddJsonFile("appsettings.json", optional: true);
